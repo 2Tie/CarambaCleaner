b93fd13a9dd6c9d882ea69f1d0a3fe9a91d808af
2Tie <foxman95@ymail.com>
2021-08-25 10:44:25 -0600

    initial commit

---
+++ CarambaCleaner/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarambaCleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            Text = "Game Window";
+
+            ClientSize = new Size(800, 600);
+
+            Image imgBuf = new Bitmap(ClientSize.Width, ClientSize.Height);
+            Graphics g = Graphics.FromImage(imgBuf);
+
+            SolidBrush b = new SolidBrush(Color.Black);
+
+            g.FillRectangle(b, ClientRectangle);
+
+            BackgroundImage = imgBuf;
+            Invalidate();
+        }
+
+    }
+}
